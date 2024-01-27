b6482cb2631f6dc06ffe80d8c838a457d477bea5
PaulKing321 <oluwadamilare321@gmail.com>
2024-01-27 22:01:36 +0100

    dependency update for stepper

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -4,17 +4,24 @@ import { AppPropsWithLayout } from '../@types';
 import theme from '../chakra';
 import { ToastContainer } from '../components/alerts/ToastContainer';
 import { SharedLayout } from '../components/layout';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+
+const muiTheme = createTheme({
+  spacing: 8,
+});
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <>
+          <ThemeProvider theme={muiTheme}>
         <ChakraProvider theme={theme}>
-              <SharedLayout>
-                <Component {...pageProps} />
-              </SharedLayout>
-          <ToastContainer />
+            <SharedLayout>
+              <Component {...pageProps} />
+            </SharedLayout>
         </ChakraProvider>
+          <ToastContainer />
+          </ThemeProvider>
     </>
   );
 }
