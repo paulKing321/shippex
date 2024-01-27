2f8912495d488de53dfc4897f82d8f7cbe770470
PaulKing321 <oluwadamilare321@gmail.com>
2024-01-27 22:04:17 +0100

    dashboard header

--- pages/dashboard/dashboardHeader.tsx
+++ pages/dashboard/dashboardHeader.tsx
@@ -3,9 +3,14 @@ import { Button, Flex } from "@chakra-ui/react"
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ITrack, TrackSchema } from "./utils";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { trackAWB } from "../../services";
+import { register } from "module";
+import { Dispatch, SetStateAction } from "react";
+// import { trackAWB } from "../../services";
 
-export const DashboardHeader = () => {
+type IProps = {
+  setShowDetails: Dispatch<SetStateAction<boolean>>;
+}
+export const DashboardHeader = ({setShowDetails}: IProps) => {
     const {
     register,
     handleSubmit,
@@ -16,28 +21,32 @@ export const DashboardHeader = () => {
   });
 
   const handleTrack: SubmitHandler<ITrack> = async (values) => {
-    try {
-      const data: any = await trackAWB(values.trackID);
-    console.log({data});
-  } catch (error) {
-    console.log({error})
-  }
+  //   try {
+  //     const data: any = await trackAWB(values.trackID);
+  //   console.log({data});
+  // } catch (error) {
+  //   console.log({error})
+    setShowDetails(true);
   };
 
     return (
-        <Flex bgColor="#F8FAFC" justifyContent="center" alignItems="center" w="100%" p={{ base: 8, md: 12 }}>
-          <Flex columnGap="20px" w="80%">
-            <InputComponent bg="white" {...register('trackID')} errorMessage={errors.trackID && errors.trackID.message} placeholder="Enter AWB ID" w="full"  />
-            <Button
-            isLoading={isSubmitting}
-            isDisabled={!!errors}
-            type="submit"
-            onClick={handleSubmit(handleTrack)}
-            bgColor="#2563EB" color="white" borderRadius="4px"
-            w="30%"
-            >
-            Track
-            </Button>
+        <Flex bgColor="#F8FAFC" justifyContent="center" alignItems="center" w="90%" p={{ base: 8, md: 12 }} height="18vh" mx="auto">
+          <Flex w={{ base: '90%', sm: '80%', md: '70%' }}>
+            <Flex columnGap="20px" w="100%">
+              {/* <Flex w="100%"> */}
+                <InputComponent bg="white" {...register('trackID')} errorMessage={errors.trackID && errors.trackID.message} placeholder="Enter AWB ID" w="100%" />
+              {/* </Flex> */}
+              <Button
+              isLoading={isSubmitting}
+              type="submit"
+              onClick={handleSubmit(handleTrack)}
+              bgColor="#2563EB" color="white" borderRadius="4px"
+              w={{ base: '30%', md: '20%' }}
+              height="40px"
+              >
+              Track
+              </Button>
+            </Flex>
           </Flex>
         </Flex>
     )
--- pages/dashboard/utils/index.ts
+++ pages/dashboard/utils/index.ts
@@ -3,7 +3,10 @@ import * as yup from 'yup';
 export interface ITrack {
   trackID: number;
 }
+export interface ITrackDetails {
+  trackID?: number;
+}
 
 export const TrackSchema = yup.object().shape({
-  trackID: yup.number().required('AWB ID is required').min(10, 'Please enter a valid AWB ID').max(13, 'Please enter a valid AWB ID'),
+  trackID: yup.string().required('AWB ID is required').min(10, 'Please enter a valid AWB ID').max(13, 'Please enter a valid AWB ID'),
 });
