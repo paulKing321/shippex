689cee1fca11685666d6ac877522e27863793046
PaulKing321 <oluwadamilare321@gmail.com>
2024-01-27 22:03:47 +0100

    dashboard page update

--- pages/dashboard/index.page.tsx
+++ pages/dashboard/index.page.tsx
@@ -1,13 +1,16 @@
+import axios from "axios";
 import { DashboardDetails } from "./dashboardDetails";
 import { DashboardHeader } from "./dashboardHeader";
+import { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
 
-const dashboard = () => {
+const Dashboard = () => {
+  const [showDetails, setShowDetails] = useState(false);
     return (
-        <>
-            <DashboardHeader />
-            <DashboardDetails />
-            
-        </>
+        <Box height="100vh" maxW="1440px" mx="auto">
+            <DashboardHeader setShowDetails={setShowDetails} />
+            <DashboardDetails showDetails={showDetails} />
+        </Box>
     )
 }
-export default dashboard;
+export default Dashboard;
