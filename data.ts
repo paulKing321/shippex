aaafbd11ce364fdae1b205905c884a2303e302b7
PaulKing321 <oluwadamilare321@gmail.com>
2024-01-27 22:02:32 +0100

    data object to improvise for API not working

---
+++ data.ts
@@ -0,0 +1,11 @@
+export const data = {
+  id: "4515645646466",
+  date: "16/12/2023",
+  time: "11:33 AM",
+  sender: "Mohamamd Manaa",
+  consignee: "Beshouy Ezzat",
+  origin_address: "Ahmed Hassan 25, Nile street Zamalek, Cairo, Egypt",
+  destination_address: "Fatima Ali, 10, orniche Road, Gleem, Alexandri, Egypt",
+  shipping_service: "Express Service",
+  total_cod_amount: "499.55 EGP",
+}
